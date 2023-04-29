12497c7e35466e0c7a80671ff4014b5b7b15c297
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-04-29 18:13:46 +0000

    Sat, Apr 29, 2023, 6:13 PM +00:00

--- functions.c
+++ functions.c
@@ -4,12 +4,12 @@ int sum(int n1, int n2);
 
 
 int main(void){
-    int x = sum(5,7);
+    int printf("Enter num1&2: %i %i",x = num1, num2);
     int result = x + 8;
     printf("%i\n", result);
 }
 
 int sum(int n1, int n2){
-    int result = n1 + n2;
+    int result = num1 + num2;
     return result;
     }
