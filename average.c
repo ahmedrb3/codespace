6a5b8d8769b83c407ff8dc2d27df077f21d3e8a5
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-01 18:42:35 +0000

    Mon, May 1, 2023, 6:42 PM +00:00

--- average.c
+++ average.c
@@ -7,6 +7,7 @@ int main(void){
     int result = 0;
     for(int i = 0; i < length; i++){
     scores[i] = get_int("number: ");
+    result += scores[i];
     }
-    printf("average %.2f\n", (scores[0] + scores[1] + scores[2])/3.0);
+    printf("average %.2f\n", result/ (float) length);
 }
