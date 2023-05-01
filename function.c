faf30d0a6201b6911179eae5be624fc8676cb8fe
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-01 11:04:56 +0000

    Mon, May 1, 2023, 11:04 AM +00:00

--- function.c
+++ function.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <cs50.h>
 
-float discount(float regular){
-    float sale = regular * .85;
-    return sale;
-}
+float discount(float regular);
 
 int main(void){
 
     float regular = get_float("regular price: ");
     float sale = regular * .85;
     printf("sale price = %.2f\n", sale);
+}
+
+float discount(float regular){
+    float sale = regular * .85;
+    return sale;
 }
