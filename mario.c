8b8db7e52a46cd74b726b69738add3b8966ca492
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-01 11:35:37 +0000

    Mon, May 1, 2023, 11:35 AM +00:00

--- mario.c
+++ mario.c
@@ -3,10 +3,11 @@
 
 int main(void){
 
-    int count = -1;
-    while(count < 1){
-    count = get_int("count: ");
-    }
+    int count;
+    do{
+          count = get_int("count: ");
+    }while(count < 1);
+
     for(int i = 0; i < count; i++){
         printf("?");
     }
