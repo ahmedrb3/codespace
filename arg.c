ae84c81e058101d19a137211cde985675c78e420
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-02 16:09:48 +0000

    Tue, May 2, 2023, 4:09 PM +00:00

--- arg.c
+++ arg.c
@@ -3,7 +3,7 @@
 
 int main(int argc, string argv[]){
     if(argc != 2){
-    printf("missin ");
+    printf("missing command line argument\n");
     return 1;
     }
     printf("hello, %s\n",agrv[1]);
