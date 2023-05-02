d7914ed7b0cb69234807cb7a4ec425a85c829361
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-02 02:13:55 +0000

    Tue, May 2, 2023, 2:13 AM +00:00

--- uppercase.c
+++ uppercase.c
@@ -7,6 +7,8 @@ int main(void){
     for(int i = 0; i < strlen(s);i++){
         if(s[i] >= 'a' && s[i] <= 'z'){
             printf("%c", s[i] - 32);
+        }else{
+            printf("%c",s[i]);
         }
     }
     printf("\n");
