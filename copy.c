685a94469249fcef9bd8465bee00ca4ab1249d10
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 17:38:41 +0000

    Wed, May 3, 2023, 5:38 PM +00:00

--- copy.c
+++ copy.c
@@ -3,8 +3,7 @@
 #include <ctype.h>
 int main(void){
     char *s = get_string("s= ");
-    char *t = s;
-
+    char *t = malloc(strlen(s) + 1);
     t[0] = toupper(t[0]);
     printf("s: %s\n", s);
     printf("t: %s\n", t);
