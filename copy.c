cd723fe8ee6f4c0b9b837e2c91b32bc42c7339be
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 17:34:16 +0000

    Wed, May 3, 2023, 5:34 PM +00:00

--- copy.c
+++ copy.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <ctype.h>
 int main(void){
-    char *
+    char *s = get_string("s= ");
+    char *t = s;
+
+    t[0] = toupper(t[0]);
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
 }
