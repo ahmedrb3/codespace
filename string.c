d2e457daf00b7fef11deeaf27dbe361279cc89af
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 12:21:36 +0000

    Wed, May 3, 2023, 12:21 PM +00:00

--- string.c
+++ string.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 //#include <cs50.h>
+typedef char * string;
 int main(void){
-    char *s = "HI!";
+    string s = "HI!";
     printf("%s\n", s);
-
 }
 //int string(void){
 //  printf("%p\n", &s[0]);
 //  printf("%p\n", &s[1]);
 //  printf("%p\n", &s[2]);
 //  }
+//////////////////////////
+//int main(void){
+//  char *s = "HI!";
+//  printf("%s\n", s);
+//}
