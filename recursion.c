7cb478312f2d44396ec4cee2b16a79f2ba93ef37
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 09:28:55 +0000

    Wed, May 3, 2023, 9:28 AM +00:00

---
+++ recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+void draw(int n);
+
+int main(void){
+    int height = get_int("height: ");
+    draw(height);
+}
+void draw(int n){
+    for(int i = 0; i < n;i++){
+        for(int j = 0; j < n;j++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
