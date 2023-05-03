146b284d48ce31dcf7d770e1a4579a71d2133746
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 01:33:40 +0000

    Wed, May 3, 2023, 1:33 AM +00:00

--- phonebook.c
+++ phonebook.c
@@ -2,12 +2,19 @@
 #include <cs50.h>
 #include <string.h>
 
-
+typedef struct {
+    string name;
+    string number;
+    
+}pb;
 
 int main(void){
 
-    string names[] = {"ahmed","devid"};
-    string numbers[] = {"012456","010123"};
+    pb people[2];
+
+    people[0].name = "ahmed";
+    people[0].number = "012456";
+
     for(int i = 0; i < 2; i++ ){
         if(strcmp(names[i], "ali") == 0){
             printf("Found %s\n", numbers[i]);
