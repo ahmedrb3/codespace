ae799a174f869ef1f5cd13b07205ebbc0fc5f8e4
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 01:05:10 +0000

    Wed, May 3, 2023, 1:05 AM +00:00

--- algo.c
+++ algo.c
@@ -3,10 +3,10 @@
 
 int main(void){
     int numbers[] = {4,6,8,2,7,5,0};
-    
+
     for(int i = 0; i < 7;i++){
         if(numbers[i] == 0){
-        printf("found\n");
+            printf("found\n");
             return 0;
         }
     }
