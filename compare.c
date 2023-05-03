93f7b2fbd44c4dbefbd05ac3a6b9baa02a57d19c
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 17:23:53 +0000

    Wed, May 3, 2023, 5:23 PM +00:00

--- compare.c
+++ compare.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void){
-    int i = get_int("i: ");
-    int j = get_int("s: ");
-    if(i == j){
+    string s = get_string("s: ");
+    string t = get_string("t: ");
+    if(s == t){
         printf("Same\n");
     }
     else{
         printf("Different\n");
     }
 }
+
+//    int i = get_int("i: ");
+//  int j = get_int("s: ");
+//  if(i == j){
+//      printf("Same\n");
+//  }
+//  else{
+//      printf("Different\n");
