013d777e92b5c7a05d12e9b79bd29f38434e7bb2
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 12:30:30 +0000

    Wed, May 3, 2023, 12:30 PM +00:00

--- string1.c
+++ string1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 int main(void){
-    string s = "HI!";
-    printf("%c\n", s[0]);
-    printf("%c\n", s[1]);
-    printf("%c\n", s[2]);
-    printf("%c\n", s[3]);
+    char *s = "HI!";
+    printf("%c\n", *s);
+    printf("%c\n", *(s+1));
+    printf("%c\n", *(s+2));
+    printf("%c\n", *(s+3));
 }
