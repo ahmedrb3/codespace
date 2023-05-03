d8cbe9a4b94f8bd56d27950576345ca104165798
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-03 18:06:31 +0000

    Wed, May 3, 2023, 6:06 PM +00:00

--- swap.c
+++ swap.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
-void swap(int a, int b);
+void swap(int *a, int *b);
 int main(void){
     int x = 1;
     int y = 2;
-    swap(x, y);
+    swap(&x, &y);
     printf("x: %i\n", x);
     printf("y: %i\n", y);
 }
-void swap(int a, int b){
-    int tmp = a;
-    a = b;
-    b = tmp;
+void swap(int *a, int *b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 //    int tmp = x;
 //    x = y;
