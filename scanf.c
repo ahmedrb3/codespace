75be74353cc4cff7d3b9ba735b7b837e7e8654d5
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 10:31:31 +0000

    Fri, May 5, 2023, 10:31 AM +00:00

--- scanf.c
+++ scanf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
+
 //#include <cs50.h>
 
 int main(void){
-    int x;
-    printf("x: ");
-    scanf("%i", &x);
-    printf("x %i \n", x);
+    char s[5];
+    printf("s: ");
+    scanf("%s", s);
+    printf("s %s \n", s);
 }
