af0338ed1c53f056f85ef5b06f0922b0d4f9864a
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 11:38:45 +0000

    Fri, May 5, 2023, 11:38 AM +00:00

---
+++ array2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int *list = malloc(3*sizeof(int));
+    if(list == NULL){
+        return 1;
+    }
+    *list = 1;
+    *(list+1) = 2;
+    *(list+2) = 3;
+}
