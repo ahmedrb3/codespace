f3424ef546cbdd9b1aadcf98f97f2240187269fd
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 10:53:37 +0000

    Fri, May 5, 2023, 10:53 AM +00:00

--- file.c
+++ file.c
@@ -5,10 +5,10 @@ int main(void){
     FILE* myfile = fopen("phonebook.csv","w");
     char* name = get_string("name: ");
     char* phone = get_string("phone: ");
-    fprintf(myfile, "%s, &s\n", name, phone);
+    fprintf(myfile, "%s, &s \n", name, phone);
     fclose(myfile);
     }
-    
+
 //mood            delete       create
 //r => read       no           no
 //w => write      yes          yes
