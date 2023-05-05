e9a751a7a417cb18d95245d86b70fd4d9f133141
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 11:47:32 +0000

    Fri, May 5, 2023, 11:47 AM +00:00

--- array2.c
+++ array2.c
@@ -9,4 +9,19 @@ int main(){
     *list = 1;
     *(list+1) = 2;
     *(list+2) = 3;
+    int *tmp = malloc(4*sizeof(int));
+    if(tmp == NULL){
+        free(list);
+        return 1;
+    }
+    for(int i = 0;i < 3;i++){
+        tmp[i] = list[i];
+    }
+    free(list);
+    tmp[3] = 4;
+    list = tmp;
+    for(int i = 0;i < 4;i++){
+        printf("%i\n", list[i]);
+    }
+    free(list);
 }
