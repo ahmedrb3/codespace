a5cca000dfca8c36c6dd8d599818b02bd1987840
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 11:31:53 +0000

    Fri, May 5, 2023, 11:31 AM +00:00

--- array.c
+++ array.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 int main(void){
-    int l[];
-    int l[0] = 1;
-    int l[1] = 2;
-    int l[2] = 3;
+    int list[3];
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
+    tmp[4];
+    for(int i = 0; i < 3;i++){
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+    for(int i = 0;i < 4;i++){
+
+    }
+}
+int sec(){
+//    int list[3];
+    int *list = malloc(3*sizeof(int));
+    if(list == NULL){
+        return 1;
+    }
+    list[0] = 1;
+    list[1] = 2;
+    list[2] = 3;
 }
