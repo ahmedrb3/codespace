f5c7b14a319901c7528ae6f53bd3431b09ef8b1d
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 11:33:26 +0000

    Fri, May 5, 2023, 11:33 AM +00:00

--- array.c
+++ array.c
@@ -11,7 +11,7 @@ int main(void){
     }
     tmp[3] = 4;
     for(int i = 0;i < 4;i++){
-
+        
     }
 }
 int sec(){
@@ -20,7 +20,7 @@ int sec(){
     if(list == NULL){
         return 1;
     }
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
+    *list = 1;
+    *(list+1) = 2;
+    *(list+2) = 3;
 }
