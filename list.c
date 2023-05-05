c0ff0f828b9c35333a0e019554d148d18f7f98eb
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-05 12:09:32 +0000

    Fri, May 5, 2023, 12:09 PM +00:00

--- list.c
+++ list.c
@@ -8,7 +8,12 @@ node;
 int main(void){
     node* list = NULL;
     node *n = malloc(sizeof(node));
+    if(n == NULL){
+        return 1;
+    }
 //    *(n).number = 1; * . == ->
     n->number = 1;
     n->next = NULL;
+    list = n;
+    n = malloc(sizeof(node));
 }
