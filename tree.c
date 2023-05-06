2d393fe666bd4ade92968f49786d2c93994cd23b
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-06 15:47:07 +0000

    Sat, May 6, 2023, 3:47 PM +00:00

--- tree.c
+++ tree.c
@@ -37,9 +37,11 @@ int main(){
 
     print_tree(tree);
 }
+
 void print_tree(node* root){
     if(root == NULL){
         return;
     }
     printf("%i\n", root->number);
+    print_tree(root->left);
 }
