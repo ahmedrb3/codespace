9c4be5521ed31debbce8421925a18aa283e604ef
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-06 15:44:23 +0000

    Sat, May 6, 2023, 3:44 PM +00:00

---
+++ tree.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+void print_tree(node* root)
+typedef struct node{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+int main(){
+    node *tree = NULL;
+    node *n = malloc(sizeof(node));
+    for(n == NULL){
+        return 1;
+    }
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
+    tree = n;
+    n = malloc(sizeof(node));
+        for(n == NULL){
+        return 1;
+    }
+    n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    tree->left = n;
+    n = malloc(sizeof(node));
+        for(n == NULL){
+        return 1;
+    }
+    n->number = 3;
+    n->left = NULL;
+    n->right = NULL;
+    tree->right = n;
+
+    print_tree(tree);
+}
+void print_tree(node* root){
+    if(root == NULL){
+        return;
+    }
+    printf("%i\n", root->number);
+}
