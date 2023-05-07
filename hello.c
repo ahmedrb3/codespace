82f6b685e45c3d27cd5b5201c225e91c89e20867
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-07 13:11:37 +0000

    Sun, May 7, 2023, 1:11 PM +00:00

--- hello.c
+++ hello.c
@@ -4,6 +4,9 @@ int main(void){
     int x = 10;
     int y = 20;
 if(x < y){
-    printf("x is less of y\n");
+    printf("x is less then y\n");
+}
+else{
+    printf("x is not less then y\n")
 }
 }
--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
-x = 10
+x = 30
 y = 20
 if x < y:
-    print("x is less y")
+    print("x is less then y")
+else:
+    print("x is not less then y")
