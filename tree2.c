213a31c8a76b06cbfec1d014ac59af8e9a21ca6e
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-07 11:57:00 +0000

    Sun, May 7, 2023, 11:57 AM +00:00

---
+++ tree2.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+typedef struct node{
+    int number;
+    struct node *left;
+    struct node *right;
+}
+node;
+
+void print_tree(node* root);
+int search(node* tree, int number);
+int main(){
+    node *tree = NULL;
+    node *n = malloc(sizeof(node));
+    if(n == NULL){
+        return 1;
+    }
+    n->number = 2;
+    n->left = NULL;
+    n->right = NULL;
+    tree = n;
+    n = malloc(sizeof(node));
+        if(n == NULL){
+        return 1;
+    }
+    n->number = 1;
+    n->left = NULL;
+    n->right = NULL;
+    tree->left = n;
+    n = malloc(sizeof(node));
+        if(n == NULL){
+        return 1;
+    }
+    n->number = 3;
+    n->left = NULL;
+    n->right = NULL;
+    tree->right = n;
+
+    search(tree, 3);
+
+}
+
+
+
+int search(node* tree, int number){
+    if(tree == NULL){
+        return 1;
+    }
+    else if(number < tree->number){
+        return search(tree->left, number);
+    }
+    else if(number > tree->number){
+        
+    }
+
+}
