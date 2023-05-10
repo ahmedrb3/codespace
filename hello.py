5fdd94539f823437c4831c69c7ac8e4efa8f6c10
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-10 11:53:32 +0000

    Wed, May 10, 2023, 11:53 AM +00:00

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
-try:
-    num1 = int(input("num1: "))
-    num2 = int(input("num2: "))
-    print(num1 + num2)
-except ValueError:
-    print("the value is not an int")
-    exit()
+def hello():
+    print("hello world")
+
+hello()
+
+def name(name):
+    print(f"hello, {name}")
+
+name("ahmed")
