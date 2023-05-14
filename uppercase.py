c1ae1a2febe51860eade473ce1932fef6215a324
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-05-14 15:48:56 +0000

    Sun, May 14, 2023, 3:48 PM +00:00

--- uppercase.py
+++ uppercase.py
@@ -1,5 +1,7 @@
 from cs50 import get_string
 
 before = get_string("before: ")
-print("after: "n end="")
-for i in before:
+print("after: ", end="")
+for c in before:
+    print(c.upper(), end="")
+print()
