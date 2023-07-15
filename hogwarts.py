7bfc5dd97afc0a5b830532f220e42eb0a4eab36d
ahmedrb3 <131991582+ahmedrb3@users.noreply.github.com>
2023-07-15 11:47:01 +0000

    Sat, Jul 15, 2023, 11:47 AM +00:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,3 +1,13 @@
+import csv
 
+houses = {
+    "Gryffindor":0,
+    "Ravenclaw":0,
+    "Hufflepuff":0,
+    "Slytherin":0,
+}
 
-
+with open("houses.csv","r") as file:
+    reader = csv.reader(file)
+    for i in reader:
+        print(i)
